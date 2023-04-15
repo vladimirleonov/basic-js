6be7b1c4210ada3890aa292233d05a8bab80544a
vladimir <vladimirleonov789@gmail.com>
2023-04-15 09:38:51 +0300

    feat: add recursive-depth

--- src/what-season.js
+++ src/what-season.js
@@ -12,26 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  if (!date || isNaN(date)) {
-    return 'Unable to determine the time of year!';
-  }
+  throw new NotImplementedError('Not implemented');
+  // if (!date || isNaN(date)) {
+  //   return 'Unable to determine the time of year!';
+  // }
 
-  const month = date.getMonth();
-  console.log(month);
-  if (month >= 0 && month <= 1 || month === 11) {
-    return 'winter';
-  } else if (month >= 3 && month <= 5) {
-    return 'spring';
-  } else if (month >= 6 && month <= 8) {
-    return 'summer';
-  } else if (month >= 9 && month <= 11) {
-    return 'autumn';
-  } else {
-    throw new Error('Invalid date!');
-  }
+  // const month = date.getMonth();
+  // console.log(month);
+  // if (month >= 0 && month <= 1 || month === 11) {
+  //   return 'winter';
+  // } else if (month >= 3 && month <= 5) {
+  //   return 'spring';
+  // } else if (month >= 6 && month <= 8) {
+  //   return 'summer';
+  // } else if (month >= 9 && month <= 11) {
+  //   return 'autumn';
+  // } else {
+  //   throw new Error('Invalid date!');
+  // }
 }
 
-getSeason(new Date(2020, 2, 31));
+//getSeason(new Date(2020, 2, 31));
 
 module.exports = {
   getSeason
