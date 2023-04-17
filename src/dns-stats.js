62611e02a7825bf31288668832369bc7a49bf8fc
vladimir <vladimirleonov789@gmail.com>
2023-04-17 22:17:47 +0300

    feat: dns-stat

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const stats = {};
+  for (let i = 0; i < domains.length; i++) {
+    const parts = domains[i].split('.').reverse();
+    let dns = '';
+    for (let j = 0; j < parts.length; j++) {
+      dns += `.${parts[j]}`;
+      stats[dns] = (stats[dns] || 0) + 1;
+    }
+  }
+  return stats;
 }
 
 module.exports = {
